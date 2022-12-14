e3d0ab13a612ca65d950c5ad01690d2c6442e52d
Ilham Dohaan <doha39anilham@gmail.com>
2022-12-14 11:24:57 +0700

    redesign

--- lib/page/room_category_page.dart
+++ lib/page/room_category_page.dart
@@ -12,52 +12,88 @@ class RoomCategoryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     roomCategoryCubit.getData();
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 250, 250, 250),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Category Room'),
+      ),
       body:
           Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
         Expanded(
           child: BlocBuilder<RoomCategoryCubit, RoomCategoryResult>(
               bloc: roomCategoryCubit,
               builder: (context, state) {
-                return GridView.count(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 3,
-                    mainAxisSpacing: 5,
-                    children: List.generate(state.category!.length, (index) {
-                      return InkWell(
-                        onTap: () {
-                          Navigator.of(context).pushNamed('/room-list',
-                              arguments:
-                                  state.category![index].roomCategoryCode);
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.network(
-                                'http://192.168.1.248:3001/image-room-category?name_file=${state.category![index].roomCategoryImage}',
-                                width: 230,
-                                height: 230),
-                            Text(
-                                'Nama Kategory: ${state.category![index].roomCategoryName}')
-                          ],
-                        ),
-                      );
-                    }));
+                return Container(
+                  margin: const EdgeInsets.only(left: 15, right: 15, top: 10),
+                  child: GridView.count(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 15,
+                      mainAxisSpacing: 5,
+                      childAspectRatio: 4 / 3,
+                      children: List.generate(state.category!.length, (index) {
+                        return InkWell(
+                          onTap: () {
+                            Navigator.of(context).pushNamed('/room-list',
+                                arguments:
+                                    state.category![index].roomCategoryCode);
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border:
+                                  Border.all(color: Colors.black54, width: 0.3),
+                              borderRadius: BorderRadius.circular(10),
+                              // color: Colors.grey
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(10),
+                                  child: AspectRatio(
+                                    aspectRatio: 16 / 9,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: Image.network(
+                                        'http://192.168.1.248:3001/image-room-category?name_file=${state.category![index].roomCategoryImage}',
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  '${state.category![index].roomCategoryName}',
+                                  style: const TextStyle(
+                                      color: Colors.black, fontSize: 15),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      })),
+                );
               }),
         ),
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('Kembali')),
-            ElevatedButton(
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll<Color>(Colors.red),
+                ),
                 onPressed: () {
                   Navigator.of(context).pushNamedAndRemoveUntil(
                       '/splash', (Route<dynamic> route) => false);
                 },
                 child: const Text('Batal')),
+            ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStatePropertyAll<Color>(Colors.lime.shade800),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Kembali')),
           ],
         )
       ]),
--- lib/page/room_list_page.dart
+++ lib/page/room_list_page.dart
@@ -13,6 +13,8 @@ class RoomListPage extends StatelessWidget {
     final roomCategoryArgs = ModalRoute.of(context)!.settings.arguments;
     roomListCubit.getData(roomCategoryArgs);
     return Scaffold(
+      appBar: AppBar(
+          automaticallyImplyLeading: false, title: Text('$roomCategoryArgs')),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -22,40 +24,92 @@ class RoomListPage extends StatelessWidget {
                 return Expanded(
                     child: GridView.count(
                         crossAxisCount: 2,
-                        crossAxisSpacing: 3,
+                        crossAxisSpacing: 10,
                         mainAxisSpacing: 5,
+                        childAspectRatio: 4 / 3,
                         children: List.generate(state.room!.length, (index) {
                           return InkWell(
                             onTap: () {
                               Navigator.of(context).pushNamed('/room-detail');
                             },
-                            child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Image.network(
-                                    'http://192.168.1.248:3001/image-room?name_file=${state.room![index].roomImage.toString()}',
-                                    width: 230,
-                                    height: 230,
-                                  ),
-                                  Text(state.room![index].roomName.toString())
-                                ]),
+                            child: Container(
+                              margin: const EdgeInsets.fromLTRB(15, 10, 15, 0),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(
+                                    color: Colors.black54, width: 0.3),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      AspectRatio(
+                                        aspectRatio: 16 / 9,
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                          child: Image.network(
+                                            'http://192.168.1.248:3001/image-room?name_file=${state.room![index].roomImage.toString()}',
+                                          ),
+                                        ),
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          const Text('Room Name: '),
+                                          Text(state.room![index].roomName
+                                              .toString())
+                                        ],
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          const Text('Room Capacity: '),
+                                          Text(
+                                              '${state.room![index].roomCapacity.toString()} PAX')
+                                        ],
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          const Text('Room Ready: '),
+                                          Text(state.room![index].roomIsReady
+                                              .toString())
+                                        ],
+                                      )
+                                    ]),
+                              ),
+                            ),
                           );
                         })));
               }),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text('Kembali')),
-              ElevatedButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.red),
+                  ),
                   onPressed: () {
                     Navigator.of(context).pushNamedAndRemoveUntil(
                         '/splash', (Route<dynamic> route) => false);
                   },
                   child: const Text('Batal')),
+              ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.lime.shade800),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Kembali')),
             ],
           )
         ],
