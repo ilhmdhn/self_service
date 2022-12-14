a1d96115469a5373fe80d5b29763d6a7f47f97a7
Ilham Dohaan <doha39anilham@gmail.com>
2022-12-14 13:09:03 +0700

    carousel

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,24 @@ import 'package:self_service/page/room_category_page.dart';
 import 'package:self_service/page/room_detail_page.dart';
 import 'package:self_service/page/room_list_page.dart';
 import 'package:self_service/page/splash_screen.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await windowManager.ensureInitialized();
+
+  // WindowOptions windowOptions = const WindowOptions(
+  //     size: Size(1366, 768),
+  //     center: true,
+  //     backgroundColor: Colors.transparent,
+  //     skipTaskbar: false,
+  //     titleBarStyle: TitleBarStyle.hidden,
+  //     fullScreen: true);
+  // windowManager.waitUntilReadyToShow(windowOptions, () async {
+  //   await windowManager.show();
+  //   await windowManager.focus();
+  // });
+
   runApp(const SelfService());
 }
 
@@ -20,8 +36,8 @@ class SelfService extends StatelessWidget {
         '/splash': (context) => const SplashPage(),
         '/reservation': (context) => const ReservationPage(),
         '/room-category': (context) => RoomCategoryPage(),
-        '/room-list': (context) =>  RoomListPage(),
-        '/room-detail': (context) =>  RoomDetailPage(),
+        '/room-list': (context) => RoomListPage(),
+        '/room-detail': (context) => RoomDetailPage(),
       },
     );
   }
--- lib/page/room_detail_page.dart
+++ lib/page/room_detail_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:self_service/bloc/room_detail_bloc.dart';
 import 'package:self_service/data/model/room_detail_model.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class RoomDetailPage extends StatelessWidget {
   RoomDetailPage({super.key});
@@ -21,31 +22,58 @@ class RoomDetailPage extends StatelessWidget {
                 return Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Container(
-                      width: double.infinity,
-                      margin: const EdgeInsets.all(10),
-                      child: AspectRatio(
-                        aspectRatio: 16 / 9,
-                        child: ListView.separated(
-                          scrollDirection: Axis.horizontal,
-                          separatorBuilder: (context, index) => const Divider(),
-                          itemCount: state.data?.roomGallery?.length ?? 0,
-                          itemBuilder: (BuildContext context, int index) {
-                            return ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.network(
-                                'http://192.168.1.248:3001/image-room?name_file=${state.data?.roomGallery?[index].imageUrl.toString()}',
-                                fit: BoxFit.contain,
-                              ),
-                            );
-                          },
-                        ),
-                      ),
+                    CarouselSlider.builder(
+                      options: CarouselOptions(
+                          height: 400,
+                          aspectRatio: 16 / 9,
+                          viewportFraction: 0.8,
+                          initialPage: 0,
+                          enableInfiniteScroll: true,
+                          reverse: false,
+                          autoPlay: true,
+                          autoPlayInterval: const Duration(seconds: 3),
+                          autoPlayAnimationDuration:
+                              const Duration(milliseconds: 800),
+                          autoPlayCurve: Curves.fastOutSlowIn,
+                          enlargeCenterPage: true,
+                          enlargeFactor: 0.3,
+                          scrollDirection: Axis.horizontal),
+                      itemCount: state.data?.roomGallery?.length ?? 0,
+                      itemBuilder: ((context, index, realIndex) => ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.network(
+                              'http://192.168.1.248:3001/image-room?name_file=${state.data?.roomGallery?[index].imageUrl.toString()}',
+                            ),
+                          )),
                     ),
                     Text('${state.data?.roomGallery?[0].imageUrl}'),
                   ],
                 );
-              })
+              }),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.red),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                        '/splash', (Route<dynamic> route) => false);
+                  },
+                  child: const Text('Batal')),
+              ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.lime.shade800),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Kembali')),
+            ],
+          )
         ],
       ),
     );
