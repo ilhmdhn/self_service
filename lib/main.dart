d18b167f543aa9a2a90e58f3fd55e7f673a74bd4
Ilham Dohaan <doha39anilham@gmail.com>
2022-12-14 13:39:44 +0700

    turn on window manager

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,20 @@ import 'package:self_service/page/splash_screen.dart';
 import 'package:window_manager/window_manager.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await windowManager.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
 
-  // WindowOptions windowOptions = const WindowOptions(
-  //     size: Size(1366, 768),
-  //     center: true,
-  //     backgroundColor: Colors.transparent,
-  //     skipTaskbar: false,
-  //     titleBarStyle: TitleBarStyle.hidden,
-  //     fullScreen: true);
-  // windowManager.waitUntilReadyToShow(windowOptions, () async {
-  //   await windowManager.show();
-  //   await windowManager.focus();
-  // });
+  WindowOptions windowOptions = const WindowOptions(
+      size: Size(1366, 768),
+      center: true,
+      backgroundColor: Colors.transparent,
+      skipTaskbar: false,
+      titleBarStyle: TitleBarStyle.hidden,
+      fullScreen: true);
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
 
   runApp(const SelfService());
 }
