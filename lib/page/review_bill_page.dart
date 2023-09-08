e799f23af6d276f175f76b27122291cdbb9e446d
Ilham Dohaan <doha39anilham@gmail.com>
2023-09-08 17:08:25 +0700

    ben ijo ae

--- lib/page/review_bill_page.dart
+++ lib/page/review_bill_page.dart
@@ -10,7 +10,7 @@ class ReviewBillPage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Expanded(
+          const Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
